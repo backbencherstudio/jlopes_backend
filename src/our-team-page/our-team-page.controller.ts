7a63dd98d7702a8923020a8a7a95688a07b0ffde
svdNadim <nadimcottonsdt@gmail.com>
2025-11-14 15:03:45 +0600

    create and find all team members

--- src/our-team-page/our-team-page.controller.ts
+++ src/our-team-page/our-team-page.controller.ts
@@ -6,25 +6,60 @@ import {
   Param,
   Patch,
   Post,
+  Query,
+  Req,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { memoryStorage } from 'multer';
 import { CreateOurTeamPageDto } from './dto/create-our-team-page.dto';
 import { UpdateOurTeamPageDto } from './dto/update-our-team-page.dto';
 import { OurTeamPageService } from './our-team-page.service';
-
-@Controller('our-team-page')
+import { Request } from 'express';
+@ApiTags('teams')
+@Controller('teams')
 export class OurTeamPageController {
   constructor(private readonly ourTeamPageService: OurTeamPageService) {}
 
+  @ApiOperation({ summary: 'Create a team member' })
   @Post()
-  create(@Body() data: CreateOurTeamPageDto) {
-    return this.ourTeamPageService.create(data);
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: memoryStorage(),
+    }),
+  )
+  async create(
+    @Body() data: CreateOurTeamPageDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    try {
+      return await this.ourTeamPageService.create(data, image);
+    } catch (error) {
+      return {
+        success: false,
+        message: error?.message || 'Something went wrong',
+      };
+    }
   }
 
+  @ApiOperation({ summary: 'Get all team members' })
   @Get()
-  findAll() {
-    return this.ourTeamPageService.findAll();
+  async findAll(@Query() query: any, @Req() req: Request) {
+    try {
+      const page = parseInt(query.page) || 1;
+      const limit = parseInt(query.limit) || 3;
+      return await this.ourTeamPageService.findAll(page, limit);
+    } catch (error) {
+      return {
+        success: false,
+        message: error?.message || 'Something went wrong',
+      };
+    }
   }
 
+  @ApiOperation({ summary: 'Get single team member' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.ourTeamPageService.findOne(+id);
--- src/our-team-page/our-team-page.service.ts
+++ src/our-team-page/our-team-page.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { StringHelper } from 'src/common/helper/string.helper';
+import { SojebStorage } from 'src/common/lib/Disk/SojebStorage';
+import appConfig from 'src/config/app.config';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateOurTeamPageDto } from './dto/create-our-team-page.dto';
 import { UpdateOurTeamPageDto } from './dto/update-our-team-page.dto';
@@ -7,13 +10,36 @@ import { UpdateOurTeamPageDto } from './dto/update-our-team-page.dto';
 export class OurTeamPageService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(data: CreateOurTeamPageDto) {
+  async create(
+    createTeamDTO: CreateOurTeamPageDto,
+    image: Express.Multer.File,
+  ) {
     try {
+      const data: any = {};
+      if (createTeamDTO.name) {
+        data.name = createTeamDTO.name;
+      }
+      if (createTeamDTO.title) {
+        data.title = createTeamDTO.title;
+      }
+      if (createTeamDTO.description) {
+        data.description = createTeamDTO.description;
+      }
+
+      if (image) {
+        // upload file
+        const filename = `${StringHelper.randomString()}${image.originalname}`;
+        await SojebStorage.put(
+          appConfig().storageUrl.avatar + filename,
+          image.buffer,
+        );
+
+        data.avatar = filename;
+      }
+
       const result = await this.prisma.team.create({
         data: {
-          name: data.name,
-          title: data.title,
-          description: data.description,
+          ...data,
         },
       });
 
@@ -30,8 +56,42 @@ export class OurTeamPageService {
     }
   }
 
-  findAll() {
-    return `This action returns all ourTeamPage`;
+  async findAll(page: number, limit: number) {
+    console.log("🚀 ~ OurTeamPageService ~ findAll ~ limit:", limit)
+    console.log("🚀 ~ OurTeamPageService ~ findAll ~ page:", page)
+    
+    try {
+      const [result, totalCount] = await this.prisma.$transaction([
+        // Implement pagination
+        this.prisma.team.findMany({
+          take: limit,
+          skip: (page - 1) * limit,
+        }),
+
+        // Count the records
+        this.prisma.team.count(),
+      ]);
+
+      // Calculate total pages
+      const totalpages = Math.ceil(totalCount / limit);
+
+      return {
+        success: true,
+        message: 'Alll team members are successfully',
+        metaData: {
+          currentPage: page,
+          totalpages,
+          totalCount,
+          limit,
+        },
+        data: result,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error?.message || 'Something went wrong',
+      };
+    }
   }
 
   findOne(id: number) {
